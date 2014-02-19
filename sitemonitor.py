851c040490a74727baf5069af06286257d22bba3
Adam Taylor <ataylor32@gmail.com>
2014-02-18 21:02:34 -0700

    Cleaned up "sitemonitor.py" so that it conforms with some PEP 8 rules
    I only addressed the errors that would not change the code all that
    much. There are still these PEP 8 errors:
    
    E128: continuation line under-indented for visual indent
    E401: multiple imports on one line
    E501: line too long (> 79 characters)

--- sitemonitor.py
+++ sitemonitor.py
@@ -2,11 +2,12 @@
 
 # sample usage: checksites.py eriwen.com nixtutor.com yoursite.org
 
-import pickle, os, sys, logging, time, urllib2, re
+import pickle, os, logging, time, urllib2, re
 from optparse import OptionParser, OptionValueError
 from smtplib import SMTP
 from getpass import getuser
 from socket import gethostname
+
 
 def generate_email_alerter(to_addrs, from_addr=None, use_gmail=False,
         username=None, password=None, hostname=None, port=25):
@@ -24,27 +25,28 @@ def generate_email_alerter(to_addrs, from_addr=None, use_gmail=False,
         if hostname:
             server = SMTP(hostname, port)
         else:
-            server = SMTP()            
+            server = SMTP()
         server.connect()
-    
+
     if username and password:
         server.login(username, password)
 
-        
     def email_alerter(message, subject='You have an alert'):
         server.sendmail(from_addr, to_addrs, 'To: %s\r\nFrom: %s\r\nSubject: %s\r\n\r\n%s' % (", ".join(to_addrs), from_addr, subject, message))
 
     return email_alerter, server.quit
 
+
 def get_site_status(url):
     try:
-        urlfile = urllib2.urlopen(url);
+        urlfile = urllib2.urlopen(url)
         status_code = urlfile.code
-        if status_code in (200,302):
+        if status_code in (200, 302):
             return 'up', urlfile
     except:
         pass
     return 'down', None
+
 
 def get_headers(url):
     '''Gets all headers from URL request and returns'''
@@ -52,6 +54,7 @@ def get_headers(url):
         return urllib2.urlopen(url).info().headers
     except:
         return 'Headers unavailable'
+
 
 def compare_site_status(prev_results, alerter):
     '''Report changed status based on previous results'''
@@ -61,28 +64,30 @@ def compare_site_status(prev_results, alerter):
         status, urlfile = get_site_status(url)
         endTime = time.time()
         elapsedTime = endTime - startTime
-        msg = "%s took %s" % (url,elapsedTime)
+        msg = "%s took %s" % (url, elapsedTime)
         logging.info(msg)
-        
-        if status != "up": elapsedTime = -1
-        
+
+        if status != "up":
+            elapsedTime = -1
+
         friendly_status = '%s is %s. Response time: %s' % (url, status, elapsedTime)
         print friendly_status
         if url in prev_results and prev_results[url]['status'] != status:
             logging.warning(status)
             # Email status messages
             alerter(str(get_headers(url)), friendly_status)
-        
+
         # Create dictionary for url if one doesn't exist (first time url was checked)
         if url not in prev_results:
             prev_results[url] = {}
-            
+
         # Save results for later pickling and utility use
         prev_results[url]['status'] = status
-        prev_results[url]['headers'] = None if urlfile == None else urlfile.info().headers 
+        prev_results[url]['headers'] = None if urlfile is None else urlfile.info().headers
         prev_results[url]['rtime'] = elapsedTime
 
     return is_status_changed
+
 
 def is_internet_reachable():
     '''Checks Google then Yahoo just in case one is down'''
@@ -92,6 +97,7 @@ def is_internet_reachable():
         return False
     return True
 
+
 def load_old_results(file_path):
     '''Attempts to load most recent results'''
     pickledata = {}
@@ -100,6 +106,7 @@ def load_old_results(file_path):
         pickledata = pickle.load(picklefile)
         picklefile.close()
     return pickledata
+
 
 def store_results(file_path, data):
     '''Pickles results to compare on next run'''
@@ -114,6 +121,7 @@ def normalize_url(url):
         url = 'http://' + url
     return url
 
+
 def get_urls_from_file(filename):
     try:
         f = open(filename, 'r')
@@ -127,18 +135,19 @@ def get_urls_from_file(filename):
         logging.error('Unable to read %s' % filename)
         return []
 
+
 def get_command_line_options():
     '''Sets up optparse and command line options'''
     usage = "Usage: %prog [options] url"
     parser = OptionParser(usage=usage)
-    parser.add_option("-t","--log-response-time", action="store_true",
+    parser.add_option("-t", "--log-response-time", action="store_true",
             dest="log_response_time",
             help="Turn on logging for response times")
 
-    parser.add_option("-r","--alert-on-slow-response", action="store_true",
+    parser.add_option("-r", "--alert-on-slow-response", action="store_true",
             help="Turn on alerts for response times")
 
-    parser.add_option("-g","--use-gmail", action="store_true", dest="use_gmail",
+    parser.add_option("-g", "--use-gmail", action="store_true", dest="use_gmail",
             help="Send email with Gmail.  Must also specify username and password")
 
     parser.add_option("--smtp-hostname", dest="smtp_hostname",
@@ -147,30 +156,31 @@ def get_command_line_options():
     parser.add_option("--smtp-port", dest="smtp_port", type="int",
             help="Set the smtp server port.")
 
-    parser.add_option("-u","--smtp-username", dest="smtp_username",
+    parser.add_option("-u", "--smtp-username", dest="smtp_username",
             help="Set the smtp username.")
 
-    parser.add_option("-p","--smtp-password", dest="smtp_password",
+    parser.add_option("-p", "--smtp-password", dest="smtp_password",
             help="Set the smtp password.")
 
-    parser.add_option("-s","--from-addr", dest="from_addr",
+    parser.add_option("-s", "--from-addr", dest="from_addr",
             help="Set the from email.")
-    
-    parser.add_option("-d","--to-addrs", dest="to_addrs", action="append",
+
+    parser.add_option("-d", "--to-addrs", dest="to_addrs", action="append",
             help="List of email addresses to send alerts to.")
 
-    parser.add_option("-f","--from-file", dest="from_file",
+    parser.add_option("-f", "--from-file", dest="from_file",
             help="Import urls from a text file. Separated by newline.")
 
     return parser.parse_args()
 
+
 def main():
 
     # Get argument flags and command options
-    (options,args) = get_command_line_options()
-    
+    (options, args) = get_command_line_options()
+
     # Print out usage if no arguments are present
-    if len(args) == 0 and options.from_file == None:
+    if len(args) == 0 and options.from_file is None:
         print 'Usage:'
         print "\tPlease specify a url like: www.google.com"
         print "\tNote: The http:// is not necessary"
@@ -199,11 +209,11 @@ def main():
     # Load previous data
     pickle_file = 'data.pkl'
     pickledata = load_old_results(pickle_file)
-        
+
     # Add some metadata to pickle
     pickledata['meta'] = {}    # Intentionally overwrite past metadata
     pickledata['meta']['lastcheck'] = time.strftime('%Y-%m-%d %H:%M:%S')
-    
+
     # create an alerter
     alerter, quiter = generate_email_alerter(options.to_addrs, from_addr=options.from_addr,
                           use_gmail=options.use_gmail,
@@ -216,7 +226,7 @@ def main():
         map(status_checker, urls)
     else:
         logging.error('Either the world ended or we are not connected to the net.')
-    
+
     # Store results in pickle file
     store_results(pickle_file, pickledata)
 
@@ -225,4 +235,3 @@ def main():
 if __name__ == '__main__':
     # First arg is script name, skip it
     main()
-
